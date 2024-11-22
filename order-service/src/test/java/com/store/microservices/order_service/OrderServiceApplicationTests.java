28a90ce7755d384401340fc4643f6a0b7affedc2
Kavindu Dilshan Arambage <157782852+arambagekd@users.noreply.github.com>
2024-11-23 02:04:02 +0530

    change test classes

--- order-service/src/test/java/com/store/microservices/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/store/microservices/order_service/OrderServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.store.microservices.order_service;
 
+
 import com.store.microservices.order_service.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
@@ -9,52 +10,57 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MySQLContainer;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
-    @ServiceConnection
-    static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
-    @LocalServerPort
-    private Integer port;
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql");
+	@LocalServerPort
+	private Integer port;
 
-    @BeforeEach
-    void setup() {
-        RestAssured.baseURI = "http://localhost";
-        RestAssured.port = port;
-    }
+	@BeforeEach
+	void setup()
+	{
+		RestAssured.baseURI="http://localhost";
+		RestAssured.port=port;
+	}
 
-    static {
-        mySQLContainer.start();
-    }
+	static {
+		mySQLContainer.start();
 
-    @Test
-    void shouldSubmitOrder() {
-        String submitOrderJson = """
-                {
-                     "skuCode": "iphone_15",
-                     "price": 1000,
-                     "quantity": 1
-                }
-                """;
-        InventoryClientStub.stubInventoryCall("iphone_15", 1);
+	}
+	@Test
+	void shouldCreateProduct() {
+		String requestBody = """
+						{
+				      "skuCode":"iphone_15",
+				      "price":1000,
+				      "quantity":1
+				  }
+					   
+				""";
+		InventoryClientStub.stubInventoryCall("iphone_15",1);
 
+		var responseBodyString =RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.extract()
+				.body().asString();
 
-        var responseBodyString = RestAssured.given()
-                .contentType("application/json")
-                .body(submitOrderJson)
-                .when()
-                .post("/api/order")
-                .then()
-                .log().all()
-                .statusCode(201)
-                .extract()
-                .body().asString();
+		assertThat(responseBodyString,Matchers.is("Order placed successfully"));
 
-        assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
-    }
+	}
+
 }
